7e0ee933273761855ed23ea84a033f244bdbc7bc
eduardo <eduardo.silvamachado07@gmail.com>
2025-09-17 22:48:15 -0300

    separa repo em duas pastas, e configura client

---
+++ client/src/App.tsx
@@ -0,0 +1,89 @@
+import { NavLink } from "react-router";
+
+export interface IStudies {
+  id?: string;
+  title: string;
+  description: string;
+  thumbnailUrl: string;
+  body: string;
+  author: string;
+  slug: string;
+}
+
+const studies: IStudies[] = [
+  {
+    title: "title 1",
+    description: "description",
+    thumbnailUrl: "thumbnailUrl",
+    body: "",
+    author: "thumbnailUrl",
+    slug: "slug",
+  },
+  {
+    title: "title 2",
+    description: "description",
+    thumbnailUrl: "thumbnailUrl",
+    body: "",
+    author: "thumbnailUrl",
+    slug: "slug",
+  },
+  {
+    title: "title 3",
+    description: "description",
+    thumbnailUrl: "thumbnailUrl",
+    body: "",
+    author: "thumbnailUrl",
+    slug: "slug",
+  },
+];
+
+function App() {
+  return (
+    <>
+      <main className=" px-20">
+        <header className="flex justify-between items-center py-6">
+          <h1 className="font-bold text-2xl">Estudos DPI</h1>
+          <nav>
+            <ul className="flex gap-4">
+              <NavLink to=''>item</NavLink>
+              <NavLink to=''>item</NavLink>
+              <NavLink to=''>item</NavLink>
+            </ul>
+          </nav>
+        </header>
+
+        <div className="grid grid-cols-3 gap-10">
+          {studies.map((study, index) => (
+            <section key={index} className="border rounded-md">
+              <section>
+                <img src={study.thumbnailUrl} alt="sorry, internal problems" className="overflow-hidden" />
+              </section>
+              <div className="p-2 flex flex-col gap-1">
+                <h1 className="capitalize font-bold">{study.title}</h1>
+                <h3 className="font-semibold">{study.author}</h3>
+                <p>{study.description}</p>
+                <NavLink to={`${study.slug}`}>
+                  <button>read</button>
+                </NavLink>
+              </div>
+            </section>
+          ))}
+        </div>
+
+        {/* <footer>
+          <h1>Estudos DPI</h1>
+          <nav>
+            <ul>
+              <li>item</li>
+              <li>item</li>
+              <li>item</li>
+            </ul>
+          </nav>
+          <a target="_blank" href="https://eduardo-machado.vercel.app/home">Desenvolvido por Eduardo Machado</a>
+        </footer> */}
+      </main>
+    </>
+  );
+}
+
+export default App;
